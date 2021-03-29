939be7b44edb4bae50042a3d260a29bcda2021df
Zabilsya <72095428+Zabilsya@users.noreply.github.com>
2021-03-30 01:29:36 +0500

    добавление страницы Помощь

--- client/src/pages/PostgraduatesInfoPage/PostgraduatesTable/PostgraduatesTableElement/PostgraduatesTableElement.js
+++ client/src/pages/PostgraduatesInfoPage/PostgraduatesTable/PostgraduatesTableElement/PostgraduatesTableElement.js
@@ -8,8 +8,8 @@ export const PostgraduatesTableElement = ({student, onChooseStudent, chosenStude
     return (
         <tr className={chosenStudent ? chosenStudent._id === _id ? "student-info chosen-student" : "student-info" : "student-info" } onClick={onChooseStudent}>
             <td className="student-info-item">{count}</td>
-            <td className="student-info-item">{name}</td>
             <td className="student-info-item">{secondName}</td>
+            <td className="student-info-item">{name}</td>
             <td className="student-info-item">{thirdName}</td>
             <td className="student-info-item">{email}</td>
             <td className="student-info-item">{admissionYear}</td>
